eb5ce54eea2ef2ef5c048a1dcddbe06ebe2e3118
Hessel <hesselpa@live.nl>
2023-06-08 00:45:45 +0200

    project files
    basic nuxt project

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+  app: {
+    head: {
+      title: 'My portfolio',
+      htmlAttrs:{
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+
+      ], 
+      link: []
+    },
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>Hello World!</div>
+</template>
+<script lang="ts" setup>
+</script>
