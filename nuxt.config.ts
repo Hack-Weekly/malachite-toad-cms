ba0255692787ccd0f2e28fa2db11d80903f752f9
MasterOfAlune <masterofalune@gmail.com>
2023-06-08 15:53:00 +0300

    setup tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  modules: ['@nuxtjs/tailwindcss'],
   app: {
     head: {
       title: 'My portfolio',
